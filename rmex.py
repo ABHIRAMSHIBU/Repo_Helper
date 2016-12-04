73a844e0c5fb0b664a0bce605b27cdd3b9c52415
Abiram Shibu <abhiramshibu1998@gmail.com>
2016-12-04 00:03:06 +0000

    Fixed some issues and tested it!

--- rmex.py
+++ rmex.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os
-a=raw_input("Enter the filetype (eg *.lock):")
+a=input("Enter the filetype (eg *.lock):")
 line='find . -name "'+a+'" -type f -delete'
 os.system(line)
 print("Done!")
